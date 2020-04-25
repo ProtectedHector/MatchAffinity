c02cc373f716636ffcf19811edfcfc058b489f40
Hector <ba0344@gmail.com>
2020-04-25 04:30:04 +0200

    Add flags

--- src/Entity/Competition.php
+++ src/Entity/Competition.php
@@ -39,7 +39,7 @@ class Competition
     private $multianual;
 
     /**
-     * @ORM\Column(type="string", length=32, nullable=true)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $flagPath;
 
